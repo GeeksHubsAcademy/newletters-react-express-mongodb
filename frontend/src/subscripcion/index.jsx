53e7f1bfe6a3e9bdbddc934d83d9e279e795b222
Diego dos Santos <diegodossantos.es@gmail.com>
2019-05-17 13:14:38 +0200

    css added

--- frontend/src/subscripcion/index.jsx
+++ frontend/src/subscripcion/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import './subscription.css';
 
 function subscripcion (){
 
        return (
 
-              <div>
-                     <h1> Subscribete en nuestra NewsLeter </h1>
+              <div id="bg">
+                     <h1>Newsletter</h1>
+                     <h6>subscribete</h6>
                      <input type="email" placeholder="pon tu email"></input>
                      <input type="submit" value="enviar"></input>
                      <input type="radio" required></input>
